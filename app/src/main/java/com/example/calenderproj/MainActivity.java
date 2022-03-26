6c44afa682f88691384015600f659da9e234ca89
kgunwoo26 <khaerim41@naver.com>
2022-03-26 14:12:37 +0900

    add: CalenderButton

--- app/src/main/java/com/example/calenderproj/MainActivity.java
+++ app/src/main/java/com/example/calenderproj/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.calenderproj;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView yearMonthText;
+    private LocalDate selectedDate;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWidgets();
+        initBtnListners();
+        selectedDate = LocalDate.now();
+        setMonthView();
+    }
+
+    private void initWidgets(){
+        yearMonthText = findViewById(R.id.YearMonthText);
+    }
+
+    private void initBtnListners() {
+        Button preButton = (Button)findViewById(R.id.preMonthBnt);
+        Button nextButton = (Button)findViewById(R.id.nextMonthBtn);
+        preButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                selectedDate= selectedDate.minusMonths(1);
+                setMonthView();
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                selectedDate= selectedDate.plusMonths(1);
+                setMonthView();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setMonthView() {
+        yearMonthText.setText(DateToString(selectedDate));
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private  String DateToString(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월");
+        return date.format(formatter);
     }
 }
