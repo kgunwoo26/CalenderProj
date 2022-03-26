7de32e1749bac475f7790c87083e476c48c1e1b4
HyukSooKim <kkhhss100960@gmailc.om>
2022-03-26 15:01:34 +0900

    중간코드수정#1
    중간코드수정#1

--- app/src/main/java/com/example/calenderproj/MainActivity.java
+++ app/src/main/java/com/example/calenderproj/MainActivity.java
@@ -11,21 +11,34 @@ import android.widget.TextView;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Calendar 클래스 : 운영체제의 날짜의 시간을 얻을때 사용
+    Calendar cal;
+
+
     private TextView yearMonthText;
     private LocalDate selectedDate;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidgets();
         initBtnListners();
         selectedDate = LocalDate.now();
         setMonthView();
+
+        // Calendar 객체 생성
+        // getInstance() 메소드 : 현재 운영체제 설정되어 있는 시간대를 기준으로
+        // Calendar 클래스의 하위 객체를 반환 (Calendar는 추상클래스)
+        cal = Calendar.getInstance();
     }
+
 
     private void initWidgets(){
         yearMonthText = findViewById(R.id.YearMonthText);
