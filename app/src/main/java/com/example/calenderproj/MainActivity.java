b1df94ebbe11e51dcdb5f17db7188fb1820b6867
HyukSooKim <kkhhss100960@gmailc.om>
2022-03-27 13:49:08 +0900

    중간코드수정#3

--- app/src/main/java/com/example/calenderproj/MainActivity.java
+++ app/src/main/java/com/example/calenderproj/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Calendar selectedDate;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -40,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initBtnListners() {
-        Button preButton = (Button)findViewById(R.id.preMonthBnt);
+        Button preButton = (Button)findViewById(R.id.preMonthBtn);
         Button nextButton = (Button)findViewById(R.id.nextMonthBtn);
+
         preButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 setMonthView();
             }
         });
+
         nextButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
@@ -61,6 +65,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+// SimpleDataFormat 날짜에 대한 형식 문자열을 설정해주는 클래스
+// 	getTime()메소드는 현재의 객체(Calendar)를 Date 객체로 변환한다.
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String DateToString(Calendar date){
+        SimpleDateFormat dataFormat =  new SimpleDateFormat("yyyy년 MM월");
+        return dataFormat.format(date.getTime());
+    }
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setMonthView() {
@@ -68,27 +80,44 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> dateArr = setCalendarDate(selectedDate);
         CalendarAdapter adapter = new CalendarAdapter(this,R.layout.item, dateArr);
         GridView mGridView = (GridView) findViewById(R.id.gridview);
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View vClicked,
+                                    int position, long id) {
+                //   String name = (String) ((TextView)vClicked.findViewById(R.id.textItem1)).getText();
+                String selected_date = adapter.getItem(position);
+                int selected_month = selectedDate.get(Calendar.MONTH) + 1;
+                int selected_year = selectedDate.get(Calendar.YEAR);
+                Toast.makeText(MainActivity.this, selected_year+"."+selected_month+"."+selected_date ,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         mGridView.setAdapter(adapter);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private String DateToString(Calendar date){
-        SimpleDateFormat dataFormat =  new SimpleDateFormat("yyyy년 MM월");
-        return dataFormat.format(date.getTime());
-    }
 
+
+// 일요일 : 0 ~ 토요일 : 6
     public ArrayList<String> setCalendarDate(Calendar date){
         ArrayList <String> dateArr = new ArrayList();
         Calendar cal = date;
         cal.set(Calendar.DATE,1);
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        dayOfWeek -=1;
-        if(dayOfWeek ==6) dayOfWeek=0;
-        int lengthOfMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)-1;
+        if(dayOfWeek ==7)
+            dayOfWeek=0;
+
+        int lengthOfMonth = cal.getActualMaximum(Calendar.DATE);
+
         for (int i = 1; i <= 41; i++) {
-            if(i<= dayOfWeek || i> (lengthOfMonth + dayOfWeek)) dateArr.add("");
-            else dateArr.add(String.valueOf(i-dayOfWeek));
+            if(i<= dayOfWeek || i> (lengthOfMonth + dayOfWeek))
+                dateArr.add("");
+            else
+                dateArr.add(String.valueOf(i-dayOfWeek));
         }
         return dateArr;
     }
+
+
+
 }
